c9a65445ee67dc2d2debeb305649e4cf86c00dcd
usman.tanveer <usman.tanveer@emumba.com>
2022-05-19 10:30:44 +0500

    Added yaml files

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ from redis import Redis
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
+
 @app.route('/')
 def hello():
     count = redis.incr('hits')
     return 'Hello from Docker! I have been seen {} times.\n'.format(count)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
